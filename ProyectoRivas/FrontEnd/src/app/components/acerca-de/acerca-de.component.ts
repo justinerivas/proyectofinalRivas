f296c3eb5020efdaba4ff99fb774fe7a3fde13e1
justine rivas <rivasjustined@gmail.com>
2022-07-21 19:41:21 -0300

    creacion de carpeta model - conexion backend con frontend - falta arreglar error get

--- ProyectoRivas/FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
+++ ProyectoRivas/FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
 
 @Component({
   selector: 'app-acerca-de',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent implements OnInit {
-
-  constructor() { }
+  persona: persona = new persona("","","");
+  
+  constructor(public personaService: PersonaService) { }
 
   ngOnInit(): void {
+    this.personaService.getPersona().subscribe(data => {this.persona = data})
   }
 
 }
