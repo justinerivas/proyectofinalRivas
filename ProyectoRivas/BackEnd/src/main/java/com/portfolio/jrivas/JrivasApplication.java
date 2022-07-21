eaad3bcab54cefa427138667fdaf1326d1371e25
justine rivas <rivasjustined@gmail.com>
2022-07-21 16:08:16 -0300

    agregando backend hasta netbeans ya probado con postman

---
+++ ProyectoRivas/BackEnd/src/main/java/com/portfolio/jrivas/JrivasApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jrivas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrivasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrivasApplication.class, args);
+	}
+
+}
---
+++ ProyectoRivas/BackEnd/src/test/java/com/portfolio/jrivas/JrivasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jrivas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrivasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
