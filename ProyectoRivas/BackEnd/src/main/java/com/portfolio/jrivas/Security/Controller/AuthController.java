e49add8d2986c0647be7f966d92f111a270ee284
rivasjustined <rivasjustined@gmail.com>
2022-07-25 19:44:11 -0300

    back jwt terminada a excepcion del error web security importacion tachada

---
+++ ProyectoRivas/BackEnd/src/main/java/com/portfolio/jrivas/Security/Controller/AuthController.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.jrivas.Security.Controller;
+
+import com.portfolio.jrivas.Security.Dto.JwtDto;
+import com.portfolio.jrivas.Security.Dto.LoginUsuario;
+import com.portfolio.jrivas.Security.Dto.NuevoUsuario;
+import com.portfolio.jrivas.Security.Entity.Rol;
+import com.portfolio.jrivas.Security.Entity.Usuario;
+import com.portfolio.jrivas.Security.Enums.RolNombre;
+import com.portfolio.jrivas.Security.Service.RolService;
+import com.portfolio.jrivas.Security.Service.UsuarioService;
+import com.portfolio.jrivas.Security.jwt.JwtProvider;
+import java.util.HashSet;
+import java.util.Set;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
+public class AuthController {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    RolService rolService;
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @PostMapping("/nuevo")  //creo un usuario nuevo que aunque no me piden, hago un usuario nuevo para ver como queda
+    public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity(new Mensaje("Campos mal puestos o email inválido"), HttpStatus.BAD_REQUEST);
+        }
+
+        if (usuarioService.existByNombreUsuario(nuevoUsuario.getNombreUsuario())) {
+            return new ResponseEntity(new Mensaje("Ese nombre de usuario ya existe"), HttpStatus.BAD_REQUEST);
+        }
+
+        if (usuarioService.existByEmail(nuevoUsuario.getEmail())) {
+            return new ResponseEntity(new Mensaje("Esa dirección de email ya existe"), HttpStatus.BAD_REQUEST);
+        }
+
+        Usuario usuario = new Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombre(),
+                nuevoUsuario.getEmail(), passwordEncoder.encode(nuevoUsuario.getPassword()));
+
+        Set<Rol> roles = new HashSet<>();
+        roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
+
+        if (nuevoUsuario.getRoles().contains("admin")) {
+            roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
+        }
+        usuario.setRoles(roles);
+        usuarioService.save(usuario);
+        return new ResponseEntity(new Mensaje("Usuario guardado"), HttpStatus.CREATED);
+    }
+    //por defecto todos tendrían rol user, salvo que se le agregue admin y ahí sería, admin
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
+        }
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+
+        return new ResponseEntity(jwtDto, HttpStatus.OK);
+
+    }
+}
