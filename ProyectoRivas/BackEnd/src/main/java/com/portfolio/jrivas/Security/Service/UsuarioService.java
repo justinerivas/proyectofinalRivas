0b3fad362e94ad60e8dcca37919111cac5dc5a52
rivasjustined <rivasjustined@gmail.com>
2022-07-25 14:20:45 -0300

    web token

--- ProyectoRivas/BackEnd/src/main/java/com/portfolio/jrivas/Security/Service/UsuarioService.java
+++ ProyectoRivas/BackEnd/src/main/java/com/portfolio/jrivas/Security/Service/UsuarioService.java
@@ -2,14 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.portfolio.jrivas.Security.Service;
 
+import com.portfolio.jrivas.Security.Entity.Usuario;
+import com.portfolio.jrivas.Security.Repository.iUsuarioRepository;
+import java.util.Optional;
 import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class UsuarioService {
-    
+
+    @Autowired
+    iUsuarioRepository iusuarioRepository;
+
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario) {
+        return iusuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+    //chequea si el nombre de usuario existe
+    public boolean existByNombreUsuario(String nombreUsuario) {
+        return iusuarioRepository.existByNombreUsuario(nombreUsuario);
+    }
+    //chequea si el email existe
+    public boolean existByEmail(String email) {
+        return iusuarioRepository.existByEmail(email);
+    }
+    public void save(Usuario usuario){
+        iusuarioRepository.save(usuario);
+    }
 }
