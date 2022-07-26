3237eaf67b5ba6c7abc4df3eff32994f68ce11bc
rivasjustined <rivasjustined@gmail.com>
2022-07-26 19:37:58 -0300

    compilacion existosa login con user y admin

--- ProyectoRivas/FrontEnd/src/app/app.module.ts
+++ ProyectoRivas/FrontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -36,6 +36,7 @@ import { LoginComponent } from './components/login/login.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule, 
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
