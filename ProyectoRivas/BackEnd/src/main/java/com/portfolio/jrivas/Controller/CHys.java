efad0651333f54edcfb69e4dbb17de409e400f04
rivasjustined <rivasjustined@gmail.com>
2022-09-28 17:57:05 -0300

    todo funciona perfecto hasta hys- lloro de alegría

--- ProyectoRivas/BackEnd/src/main/java/com/portfolio/jrivas/Controller/CHys.java
+++ ProyectoRivas/BackEnd/src/main/java/com/portfolio/jrivas/Controller/CHys.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://ftdjustine.web.app/")
+@CrossOrigin(origins = "https://ftdjustine.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
